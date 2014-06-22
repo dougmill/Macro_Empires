ac204b82b1e0a5075a9b0e049544ee2ed4425639
Douglas Miller <millerdouglas@bellsouth.net>
2014-06-22 14:38:30 -0700

    Test commit

--- src/main/java/com/douglas/game/Main.java
+++ src/main/java/com/douglas/game/Main.java
@@ -12,7 +12,6 @@ import java.net.URI;
  *
  */
 public class Main {
-    // test comment
     // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/myapp/";
 
